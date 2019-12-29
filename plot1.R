c268d0566111aaac56131ffd5d528569b7fc4cfa
morgan0109 <56382844+morgan0109@users.noreply.github.com>
2019-12-29 16:23:08 +0800

    Delete plot1.R

--- plot1.R
+++
@@ -1,4 +0,0 @@
-data <- read.table('/Users/lydc/datascience/EDA/household_power_consumption.txt',text = grep("^[1,2]/2/2007",value=TRUE), sep = ';', col.names = c("Date", "Time", "Global_active_power", "Global_reactive_power", "Voltage", "Global_intensity", "Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), na.strings = '?')
-png(filename = "plot1.png", width = 480, height = 480, units='px')
-hist(data$Global_active_power,xlab = 'Global Active Power (kilowatt)', main = 'Global Active Power', col = 'red')
-dev.off
